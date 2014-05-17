aa16b388758ab9608e301d5f8120e6ae8827bd84
andy5876 <andrewnind@gmail.com>
2014-05-17 20:41:35 +0100

    update to 1.1

--- volatility GUI/Program.cs
+++ volatility GUI/Program.cs
@@ -16,7 +16,7 @@ namespace volatility_GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
